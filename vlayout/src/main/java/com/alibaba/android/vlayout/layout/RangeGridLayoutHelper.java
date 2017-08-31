a8694fd6bf3b5923006d66266c3110084a1ca13e
longerian <xlhongultimate@gmail.com>
2017-08-31 09:54:38 +0800

    add comment

--- vlayout/src/main/java/com/alibaba/android/vlayout/layout/RangeGridLayoutHelper.java
+++ vlayout/src/main/java/com/alibaba/android/vlayout/layout/RangeGridLayoutHelper.java
@@ -975,6 +975,7 @@ public class RangeGridLayoutHelper extends BaseLayoutHelper {
             for (int i = 0, size = rangeStyle.mChildren.size(); i < size; i++) {
                 GridRangeStyle childRangeStyle = rangeStyle.mChildren.valueAt(i);
                 if (!childRangeStyle.isChildrenEmpty()){
+                    //FIXEME may compute the wrong start space here
                     offset += computeStartAlignOffset(childRangeStyle, layoutInVertical);
                 }else if (childRangeStyle.mRange.getLower().intValue() == startPosition) {
                     offset += (layoutInVertical ? -childRangeStyle.mMarginTop - childRangeStyle.mPaddingTop
