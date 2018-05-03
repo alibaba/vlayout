bb7a288969f70f6632e7f693f4b51fa98771ec7c
MikeAfc <mike8264791@gmail.com>
2018-05-03 11:05:45 +0800

    Make recycle offset work whether layout from start or from end.

--- vlayout/src/main/java/com/alibaba/android/vlayout/ExposeLinearLayoutManagerEx.java
+++ vlayout/src/main/java/com/alibaba/android/vlayout/ExposeLinearLayoutManagerEx.java
@@ -1155,8 +1155,7 @@ class ExposeLinearLayoutManagerEx extends LinearLayoutManager {
             }
             recycleByLayoutStateExpose(recycler, layoutState);
         }
-        int remainingSpace = layoutState.mAvailable + layoutState.mExtra + (
-            layoutState.mLayoutDirection == LayoutState.LAYOUT_START ? 0 : recycleOffset); //FIXME  opt here to fix bg and shake
+        int remainingSpace = layoutState.mAvailable + layoutState.mExtra + recycleOffset;
         while (remainingSpace > 0 && layoutState.hasMore(state)) {
             layoutChunkResultCache.resetInternal();
             layoutChunk(recycler, state, layoutState, layoutChunkResultCache);
